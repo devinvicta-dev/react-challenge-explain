f44817976883cc627da1a1d656e168b9db6ef096
devinvicta-dev <github-devinvicta.com>
2025-04-10 18:04:22 -0300

    chore: setting setup project

---
+++ models/enums/form.ts
@@ -0,0 +1,6 @@
+export enum SchemaValidationError {
+  INVALID_MINIMAL_LENGTH = "This field is required",
+  INVALID_EMAIL = "A valid email address is required",
+  INVALID_PASSWORD = "Password must be at least 6 characters long",
+}
+ 
